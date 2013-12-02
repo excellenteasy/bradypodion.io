36638ea1b038be3adb8b1aade63042693a05de74
Stephan Bönnemann <stephan@boennemann.me>
2013-12-02 22:03:59 +0100

    fix: don't lose url hash due to scrollTo fanciness

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,21 @@
 $(function(){
   $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-          || location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
 
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-             if (target.length) {
-               $('html,body').animate({
-                   scrollTop: target.offset().top
-              }, 1000);
-              return false;
-          }
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+         scrollTop: target.offset().top
+      }, 1000);
+      if (window.history != null && window.history.pushState != null) {
+        window.history.pushState({},null,this.hash);
       }
-  });
+      return false;
+    }
+  }
+});
 
   $('#onthelist').click(function(){
     setTimeout(function(){

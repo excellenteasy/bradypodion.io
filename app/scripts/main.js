0fd6c65426a5fa1937c30c05649fdafebb6b085d
Stephan Bönnemann <stephan@boennemann.me>
2013-12-02 19:36:53 +0100

    feat: scroll to top for lower signup button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('Hello, I\'m doing something');
+(function(){
+  $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+          || location.hostname == this.hostname) {
+
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+             if (target.length) {
+               $('html,body').animate({
+                   scrollTop: target.offset().top
+              }, 1000);
+              return false;
+          }
+      }
+  });
+  $('#onthelist').click(function(){
+    setTimeout(function(){
+      $('#mce-EMAIL').focus();
+    },1000);
+  });
+}).call(this);
